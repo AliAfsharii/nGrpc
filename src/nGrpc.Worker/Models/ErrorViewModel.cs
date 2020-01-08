906ad272d0a49775aea74e73c2e111b6c1af6f7b
Ali Afshari <aliafshari66@gmail.com>
2020-01-08 05:10:03 +0330

    remove mvc default files

--- src/nGrpc.Worker/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace nGrpc.Worker.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
